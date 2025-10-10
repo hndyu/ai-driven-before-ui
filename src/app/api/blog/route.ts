4520df8daf1cd0f410d412019eb05e7c5d394e75
hndyu <4225495+hndyu@users.noreply.github.com>
2025-10-10 13:20:10 +0900

    2nd commit

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "../../../generated/prisma";
+
+const prisma = new PrismaClient();
+
+export async function main() {
+    try {
+        await prisma.$connect();
+    } catch (err) {
+        return Error("DB接続に失敗しました");
+    }
+}
+
+export const GET= async (req: Request, res: Response) => {
+    try {
+        const posts = await prisma.post.findMany({ orderBy: { date: "asc" } });
+        return NextResponse.json({ message: "Success", posts }, { status: 200 });
+    } catch (err) {
+        return NextResponse.json({ message: "Error", err }, { status: 500 });
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+
+export const POST = async (req: Request, res: Response) => {
+    console.log("POST");
+
+    try {
+        const { title, description } = await req.json();
+        await main();
+        const post = await prisma.post.create({ data: { title, description } });
+        return NextResponse.json({ message: "Success", post }, { status: 201 });
+    } catch (err) {
+        return NextResponse.json({ message: "Error", err }, { status: 500 });
+    } finally {
+        await prisma.$disconnect();
+    }
+}
