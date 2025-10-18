ed1a90a4fa42a96520ac2096c200eb0686c8ebd9
hndyu <4225495+hndyu@users.noreply.github.com>
2025-10-18 11:22:02 +0900

    サインイン画面を中央寄せに変更

---
+++ src/types/clerk.d.ts
@@ -0,0 +1,11 @@
+declare module "@clerk/nextjs" {
+  import * as React from "react";
+
+  export const SignIn: React.ComponentType<any>;
+  export const SignUp: React.ComponentType<any>;
+  export const SignInButton: React.ComponentType<any>;
+  export const SignUpButton: React.ComponentType<any>;
+  export const UserButton: React.ComponentType<any>;
+
+  export default {} as any;
+}
